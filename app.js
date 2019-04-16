fa625f1b59e36116d74a701964a10fc1efeb7e05
Kaleb Delk <kdd09f@gmail.com>
2019-04-16 17:24:01 +0000

    debug MongoDB Atlas connection

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const commentRoutes    = require('./routes/comments'),
       campgroundRoutes = require('./routes/campgrounds'),
       indexRoutes      = require('./routes/index');
       
-mongoose.connect("mongodb+srv://kalebddelk:<433208kdd>@cluster0-vgubh.mongodb.net/yelp_camp?retryWrites=true");
+mongoose.connect("mongodb+srv://kalebddelk:433208kdd@cluster0-vgubh.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
